38a8a0cf1063221c88dc270d3ad13534322a988f
pereiraR3 <anthony_rodriguespereira@outlook.com>
2025-09-09 04:01:55 -0400

    fix: added interceptors in app.config

--- minha-carteira-dashboard/src/app/app.config.ts
+++ minha-carteira-dashboard/src/app/app.config.ts
@@ -1,13 +1,18 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { authInterceptor } from './core/auth/auth.interceptor';
+import { apiUrlInterceptor } from './core/auth/api-url.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes), provideClientHydration(withEventReplay()),
+    provideHttpClient(),
+    provideHttpClient(withInterceptors([apiUrlInterceptor, authInterceptor]))
   ]
 };
