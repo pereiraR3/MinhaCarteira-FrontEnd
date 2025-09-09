39989ca908562bfd48eaaf323786765927289dc8
pereiraR3 <anthony_rodriguespereira@outlook.com>
2025-09-09 03:58:32 -0400

    feat: created interceptors

---
+++ minha-carteira-dashboard/src/app/core/auth/api-url.interceptor.ts
@@ -0,0 +1,14 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+
+export const apiUrlInterceptor: HttpInterceptorFn = (req, next) => {
+  const baseUrl = 'http://localhost:8080';
+
+  if (req.url.startsWith('/api')) {
+    const apiReq = req.clone({
+      url: `${baseUrl}${req.url}`
+    });
+    return next(apiReq);
+  }
+
+  return next(req);
+};
